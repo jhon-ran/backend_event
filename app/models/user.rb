c20b120fd767bf452e3e2bae779fc786e38eb4fe
jhon-ran <jhonnatan.rangel@gmail.com>
2020-05-11 19:19:23 +0200

    added seed user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  # has_secure_password
   has_many :participations
   has_many :events, through: :attendance
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Event.destroy_all
+Attendance.destroy_all
+User.destroy_all
+
+# seed user
+5.times do
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    description: Faker::Lorem.sentence,
+    email: Faker::Name.first_name + "@yopmail.com", #Faker::Internet.email,
+    encrypted_password: 'password'
+  )
+end
+
+
+# seed Event
+
+
+# seed Attendance
+

b6e54bbd613ba1d6f768b87af3c33eebddd4b2fc
jhon-ran <jhonnatan.rangel@gmail.com>
2020-05-12 14:24:24 +0200

    changed root to events/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   
   
-  root to: "static_pages#index"
+  # root to: "static_pages#index"
   get 'static_pages/index'
   get 'static_pages/secret'
   devise_for :users
   # get 'welcome/index'
   #root to: "welcome#index"
+  root to: "events#index"
   
   
   resources :users
